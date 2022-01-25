3fcbb0f1e6be1ae2cd144c2a84c665b8d097bf84
tomasz <tomasz.feliks.mlynarczyk@gmail.com>
2022-01-25 13:31:17 +0100

    commit grupy 1

--- backend/src/main/java/com/example/workflow/TicketProcess.java
+++ backend/src/main/java/com/example/workflow/TicketProcess.java
@@ -7,6 +7,7 @@ public class TicketProcess {
     public static String PR01_ST1_SAVE_PAYMENT_IS_PAYED = "isPaymentSaved";
     public static String PR01_ET1_GET_TICKET_INFO_TICKET_TYPE = "ticketType";
     public static String PR01_MESSAGE_PAYMENT_MADE = "paymentMade";
+    public static String PR01_USER_TASK = "USER_TASK";
 
 
 }
