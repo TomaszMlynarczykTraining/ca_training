0ba2d6036a994f4f7d22410799a908d60c9b37ba
unknown <tomasz.feliks.mlynarczyk@gmail.com>
2022-01-25 10:00:03 +0100

    added object variable mapping

--- backend/src/main/java/com/example/workflow/delegate/SavePaymentDelegate.java
+++ backend/src/main/java/com/example/workflow/delegate/SavePaymentDelegate.java
@@ -1,11 +1,14 @@
 package com.example.workflow.delegate;
 
 import com.example.workflow.TicketProcess;
+import com.example.workflow.mvc.entity.Pizza;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 @Component
 public class SavePaymentDelegate implements JavaDelegate {
@@ -21,6 +24,21 @@ public class SavePaymentDelegate implements JavaDelegate {
         //flush do bazy danych
 
         delegateExecution.setVariable(TicketProcess.PR01_ST1_SAVE_PAYMENT_IS_PAYED, true);
+
+        Pizza pizza = Pizza.builder().name("Pizza").id(1).build();
+        Pizza pizza2 = Pizza.builder().name("Pizza2").id(1).build();
+
+        ArrayList<Pizza> pizzas = new ArrayList<>();
+
+        pizzas.add(pizza);
+        pizzas.add(pizza2);
+
+        delegateExecution.setVariable("pizza", pizza);
+
+        delegateExecution.setVariable("pizzaList", pizzas);
+
+        Object pizza1 = delegateExecution.getVariable("pizza");
+
 
     }
 }
